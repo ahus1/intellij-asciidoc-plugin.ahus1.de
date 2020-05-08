93c72a15ebb873c374095346e958451276c80d80
Alexander Schwartz <alexander.schwartz@gmx.net>
2020-05-08 23:24:01 +0200

    fix build script

--- build.sh
+++ build.sh
@@ -1,28 +1,3 @@
 #!/usr/bin/env bash
-set -ex
-
-yarn generate
-
-# serve site on localhost and run link checker
-# this call is optimized to run on the netlify server
-node /opt/build/repo/node_modules/.bin/http-server dist -p 5000 -a 127.0.0.1 &
-child_id=$!
-
-cd blc
-yarn install
-yarn blc
-cd ..
-
-cd pa11y
-yarn install
-yarn check-dist
-cd ..
-
-ps xu
-kill $child_id || true
-ps xu
-
-cd lambda
-yarn install
-yarn generate
-cd ..
+jekyll build --destination _site
+cp _redirects _site/_redirects
