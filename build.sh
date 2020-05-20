03e812ea1af5845041b7afde395a7b78ec958446
Alexander Schwartz <alexander.schwartz@gmx.net>
2020-05-20 21:23:36 +0200

    monkey-patch antora lunr to don't include pagination

--- build.sh
+++ build.sh
@@ -9,7 +9,11 @@ jekyll build --source startpage --destination _site
 cp _redirects _site/_redirects
 
 # prepare antora
-export DOCSEARCH_ENABLED=true && export DOCSEARCH_ENGINE=lunr && yarn install && yarn build
+export DOCSEARCH_ENABLED=true && export DOCSEARCH_ENGINE=lunr && \
+yarn install && \
+echo "monkey-patch antora-lunr for https://github.com/Mogztter/antora-lunr/pull/56 / will be part of v0.7.2+ " \
+cp _antora/generate-index.js node_modules/antora-lunr/lib && \
+yarn build
 
 # preparing lambda
 cd lambda
