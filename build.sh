57324a44d570ca159bb23bdd425a3e7860fae721
Alexander Schwartz <alexander.schwartz@gmx.net>
2020-06-07 13:36:17 +0200

    reorder redirects

--- build.sh
+++ build.sh
@@ -5,9 +5,6 @@ set -x
 # prepare start page with jekyll
 jekyll build --source startpage --destination _site
 
-# configure netlify
-cp _redirects _site/_redirects
-
 # prepare antora: enable document search; override URL environment variable as Antora "looks" at it
 export DOCSEARCH_ENABLED=true && export DOCSEARCH_ENGINE=lunr && \
 export URL=${URL}/docs && \
@@ -15,7 +12,11 @@ yarn install && \
 echo "monkey-patch antora-lunr for https://github.com/Mogztter/antora-lunr/pull/56 / will be part of v0.7.2+ " && \
 cp _antora/generate-index.js node_modules/antora-lunr/lib && \
 yarn build
+
+# configure netlify
+cat _redirects_top _site/_redirects
 cat _site/docs/_redirects >> _site/_redirects
+cat _redirects_end >> _site/_redirects
 
 # debugging
 cat _site/_redirects
